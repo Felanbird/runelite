a18e9abb3798a392bc906c1d38be46cd4834ddf0
Felanbird <41973452+Felanbird@users.noreply.github.com>
2025-07-23 21:53:44 -0400

    clues: move coordinate clue

--- runelite-client/src/main/java/net/runelite/client/plugins/cluescrolls/clues/CoordinateClue.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/cluescrolls/clues/CoordinateClue.java
@@ -593,6 +593,12 @@ public class CoordinateClue extends ClueScroll implements LocationClueScroll
 			.directions("South of Shayziens' Wall.")
 			.enemy(SARADOMIN_WIZARD)
 			.build(),
+		CoordinateClue.builder()
+			.itemId(ItemID.TRAIL_HARD_SEXTANT_EXP16)
+			.location(new WorldPoint(1321, 3323, 0))
+			.directions("South of Custodia Pass")
+			.enemy(SARADOMIN_WIZARD)
+			.build(),
 		// Elite
 		CoordinateClue.builder()
 			.itemId(ItemID.TRAIL_ELITE_SEXTANT_EXP5)
@@ -835,12 +841,7 @@ public class CoordinateClue extends ClueScroll implements LocationClueScroll
 			.directions("At the top of The Proudspire")
 			.enemy(ARMADYLEAN_OR_BANDOSIAN_GUARD)
 			.build(),
-		CoordinateClue.builder()
-			.itemId(ItemID.TRAIL_HARD_SEXTANT_EXP16)
-			.location(new WorldPoint(1321, 3323, 0))
-			.directions("South of Custodia Pass")
-			.enemy(SARADOMIN_WIZARD)
-			.build(),
+
 		// Master
 		CoordinateClue.builder()
 			.itemId(ItemID.TRAIL_CLUE_MASTER)
